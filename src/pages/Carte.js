a3881d07efd3662842e0ce264cb126d7d73bdc37
root <root@vps-9daa1b40.vps.ovh.net>
2023-10-01 19:47:24 +0000

    Add delay for dispaying cartographie

--- src/pages/Carte.js
+++ src/pages/Carte.js
@@ -7,17 +7,34 @@ import axios from "axios";
 import { AuthContext } from "../helpers/AuthContext";
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
+import PropagateLoader from "react-spinners/PropagateLoader"
 
 function Carte() {
   const [linkCartographie, setLinkCartographie] = useState({});
   const { authState } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    // Effectuez vos opérations de vérification d'URL ou autres ici
+    // Une fois les opérations terminées, définissez isLoading sur false
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Exemple de délai de chargement (2 secondes)
+  }, []);
   
   useEffect(() => {
     axios.get("http://back.r3mob.fr/infos/cartographie").then((response) => {
       setLinkCartographie(response.data);
     });
   }, []);
+
+	if (isLoading) {
+    return <PropagateLoader cssOverride={{
+      "display": "flex",
+      "justifyContent": "center", "alignItems": "center", "height": "100vh"
+    }}
+      color="#36d7b7" />
+  }
   return (
     authState.status==true ? (
 
